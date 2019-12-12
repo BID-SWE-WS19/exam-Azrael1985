b80896e6b827d3e91f7f2789d623a5ae0d97827c
Stefan Strauss <stefan@stefanstrauss.de>
2019-12-12 16:15:17 +0100

    push

--- CS/NUnitExam/UnitTest1.cs
+++ CS/NUnitExam/UnitTest1.cs
@@ -1,9 +1,13 @@
+using BID.SWE.EXAM.Impl;
 using NUnit.Framework;
+using System;
 
 namespace NUnitExam
 {
     public class Tests
     {
+        private DateTime date;
+
         [SetUp]
         public void Setup()
         {
@@ -12,8 +16,14 @@ namespace NUnitExam
         [Test]
         public void Test1()
         {
-            var x = 
-            Assert.Pass();
+            var x = new Aufgabe1(true, date, "rot", 1, 2);
+            Assert.AreEqual("Cell is on Position X:1 Y:2", x.PrintPostion(1,2));
+        }
+        [Test]
+        public void Test2()
+        {
+            var x = new Aufgabe1(true, date, "rot", 1, 2);
+            Assert.AreEqual("Cell rot born on 01.01.0001 00:00:00 is alive", x.PrintState(true, date,"rot"));
         }
     }
 }
